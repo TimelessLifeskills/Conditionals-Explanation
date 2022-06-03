cff8a31e7fd09885d46607f69e0c80cabbe88a6a
TimelessLifeskills <106376363+TimelessLifeskills@users.noreply.github.com>
2022-06-03 11:02:23 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let Marks = 0
+input.onButtonPressed(Button.A, function () {
+    Marks = 5
+    if (Marks >= 20) {
+        basic.showString("Well done!")
+    } else if (Marks >= 15) {
+        basic.showString("Good effort.")
+    } else if (Marks >= 10) {
+        basic.showString("You can do better")
+    } else {
+        basic.showString("Put more effort.")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Marks = 5
+    if (Marks <= 9) {
+        basic.showString("Put more effort.")
+    } else if (Marks <= 14) {
+        basic.showString("You can do better")
+    } else if (Marks <= 19) {
+        basic.showString("Good effort.")
+    } else {
+        basic.showString("Well done!")
+    }
+})
